1a9fb419fce399c94f8d7599fb5e50f116d07e28
Marquis Ogre <marquisogre@gmail.com>
2021-07-25 12:37:54 +0530

    Minor css fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ class Navbar extends Component {
 
   render() {
     return (
-      <nav className="navbar navbar-dark fixed-top bg-danger flex-md-nowrap p-0 shadow">
+      <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
         <a
         className="nav-item text-nowrap d-none d-sm-none d-sm-block"
         href="https://github.com/Dexorzo"
@@ -13,14 +13,17 @@ class Navbar extends Component {
         rel="noopener noreferrer"
         >
           <small className="text-light">
-            <medium id="account">{this.props.account}</medium>
+            <small id="account">{this.props.account}</small>
           </small>          
         </a>
 
+          <li className="navbar navbar-light bg-light">
+          <b>This Marquis Ogre demo project on BSC Testnet, There is no such token called DEX</b>
+          </li>
         <ul className="navbar-nav px-3">
           <li className="navbar-brand col-sm-3 col-md-2 mr-0">
             <img src={farmer} width="30" height="30" className="d-inline-block align-top" alt=""/>
-          &nbsp; <b> DEX Yield Farming </b>
+          &nbsp; <b> Dex Yield Farming </b>
           </li>
         </ul>
       </nav>
